59c5b03c275bb85d855c0ce47afec00544688471
Alejandor Daniel López Orona <128829840+Daniel-orona21@users.noreply.github.com>
2025-06-11 15:38:02 -0600

    cambios del miercoles

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -55,7 +55,7 @@ function Dashboard() {
                   <Col md="8" xs="7">
                     <div className="numbers">
                       <p className="card-category">ALUMNO 1</p>
-                      <CardTitle tag="p">JUAN PEREZ</CardTitle>
+                      <CardTitle tag="p">Alejandro Daniel López Orona</CardTitle>
                       <p />
                     </div>
                   </Col>
